761d84a9809a85a2cdd23f424d1075e31ac0f3de
astral4 <88992929+astral4@users.noreply.github.com>
2023-12-29 13:19:29 -0500

    add benchmark for four banners

---
+++ benches/four_banners.rs
@@ -0,0 +1,66 @@
+use aksim::{calculate, Banner, Float};
+use divan::{bench, black_box};
+
+fn main() {
+    divan::main();
+}
+
+#[bench]
+fn target_1() -> Float {
+    let banners = [
+        Banner {
+            target: 1,
+            subrate: 0.35,
+            bonus_pulls: 24,
+        },
+        Banner {
+            target: 1,
+            subrate: 0.5,
+            bonus_pulls: 0,
+        },
+        Banner {
+            target: 1,
+            subrate: 0.2,
+            bonus_pulls: 48,
+        },
+        Banner {
+            target: 1,
+            subrate: 0.4,
+            bonus_pulls: 0,
+        },
+    ];
+
+    let pulls = 400;
+
+    calculate(black_box(&banners), black_box(pulls))
+}
+
+#[bench]
+fn target_10() -> Float {
+    let banners = [
+        Banner {
+            target: 10,
+            subrate: 0.35,
+            bonus_pulls: 240,
+        },
+        Banner {
+            target: 10,
+            subrate: 0.5,
+            bonus_pulls: 0,
+        },
+        Banner {
+            target: 10,
+            subrate: 0.2,
+            bonus_pulls: 480,
+        },
+        Banner {
+            target: 10,
+            subrate: 0.4,
+            bonus_pulls: 0,
+        },
+    ];
+
+    let pulls = 4000;
+
+    calculate(black_box(&banners), black_box(pulls))
+}
